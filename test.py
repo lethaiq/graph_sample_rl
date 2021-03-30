75457469941fbec988955ff3c971aac659077372
“lethaiq” <leqthai.vn@gmail.com>
2021-03-30 16:51:04 -0400

    updates

--- test.py
+++ test.py
@@ -26,7 +26,7 @@ import gc
 import logging, argparse
 
 g_paths = [
-    'data/rand_data/rand_50.pkl',
+    'data/rt/copen.pkl'
     # 'data/rt/damascus.pkl',
     # 'data/rt/occupy.pkl'
 ]
@@ -220,7 +220,7 @@ replay = PriortizedReplay(BUFF_SIZE, 10, beta=0.6)
 
 acmodel = DQNTrainer(input_dim=input_dim,state_dim=action_dim, action_dim=action_dim, replayBuff=replay, lr=LR, use_cuda=use_cuda, gamma=args.gamma,
                         eta=eta, gcn_num_layers=gcn_layers, num_pooling=num_pooling, assign_dim=assign_dim, assign_hidden_dim=assign_hidden_dim)
-acmodel = torch.load('./models/rand_data100.pth')
+acmodel = torch.load('./models/sample_100.pth')
 print("loaded modelsl")
 
 #generate graph
