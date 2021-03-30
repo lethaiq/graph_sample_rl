6db0e2c3b0621eaaa41f6648f9ff492be43f3468
“lethaiq” <leqthai.vn@gmail.com>
2021-03-30 17:53:40 -0400

    updates

--- test.py
+++ test.py
@@ -220,7 +220,7 @@ replay = PriortizedReplay(BUFF_SIZE, 10, beta=0.6)
 
 acmodel = DQNTrainer(input_dim=input_dim,state_dim=action_dim, action_dim=action_dim, replayBuff=replay, lr=LR, use_cuda=use_cuda, gamma=args.gamma,
                         eta=eta, gcn_num_layers=gcn_layers, num_pooling=num_pooling, assign_dim=assign_dim, assign_hidden_dim=assign_hidden_dim)
-acmodel = torch.load('./models/sample_200.pth')
+acmodel = torch.load('./models/sample_300.pth')
 print("loaded modelsl")
 
 #generate graph
